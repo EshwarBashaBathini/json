216436607493067fdebfeec2283f14b6f1ee435f
EshwarBB <eshwar@middlewaretalents.com>
2026-01-28 17:03:55 +0530

    similar posts 20 mins

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -11,141 +11,159 @@ import PostItem from '../postItem';
 
 const Home = () => {
 
-    const {userId} = useParams()  
+    const { userId } = useParams()
     const [user, setUser] = useState({})
-    const [posts,setPost] = useState([])
+    const [posts, setPost] = useState([])
+    const [similarPosts, setSimilarPost] = useState([])
     const { email, id, name, phone, username, website, address = {}, company = {} } = user
-    const { suite, street, city, zipcode, geo={} } = address
-    const {lat, lng} = geo
+    const { suite, street, city, zipcode, geo = {} } = address
+    const { lat, lng } = geo
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
             .then(res => res.json())
             .then(data => {
                 setUser(data)
-                
+
             })
-        fetch (`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)   
+        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
             .then(res => res.json())
             .then(data => {
                 setPost(data)
-               
-            }) 
+
+            })
+        fetch(`https://jsonplaceholder.typicode.com/posts`)
+            .then(res => res.json())
+            .then(data => {
+                const filteredData = data.filter(post => (
+                    post.userId !== Number(userId)
+                ))
+                setSimilarPost(filteredData)
+
+            })
     }, [userId])
-    
+
     return (
-        <div className='display-flex'>
-            <div className='container-box'>
-                <div className='box-profile'>
-                    <h2 className='heading'>{name}</h2>
-                    <p>{email}</p>
+        <div className='top-container'>
+            <div className='display-flex'>
+                <div className='container-box'>
+                    <div className='box-profile'>
+                        <h2 className='heading'>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                    <div className='container-info'>
+                        <div className='flex'>
+                            <RiContactsFill />
+                            <h4 className='flex-head' >Basic Information</h4>
+                        </div>
+                        <div className='content'>
+                            <h3 className='content-heading' >User ID</h3>
+                            <p className='content-para'>{id}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Full Name</h3>
+                            <p className='content-para'>{name}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Username</h3>
+                            <p className='content-para'>{username}</p>
+                        </div>
+                        <hr />
+                        <div className='flex' >
+                            <FaPhoneAlt />
+                            <h4 className='flex-head'>Contact Information</h4>
+                        </div>
+                        <div className='content'>
+                            <h3 className='content-heading'>Email ID</h3>
+                            <p className='content-para'>{email}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Phone</h3>
+                            <p className='content-para'>{phone}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Website</h3>
+                            <p className='content-para'>{website}</p>
+                        </div>
+                        <hr />
+                        <div className='flex' >
+                            <IoLocationOutline />
+
+                            <h4 className='flex-head' >Address</h4>
+                        </div>
+                        <div className='content'>
+                            <h3 className='content-heading'>Street</h3>
+                            <p className='content-para'>{street}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Suite</h3>
+                            <p className='content-para'>{suite}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>City</h3>
+                            <p className='content-para'>{city}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Zipcode</h3>
+                            <p className='content-para'>{zipcode}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Coordinates</h3>
+                            <ul className='content-heading'><li className='content-para'>
+                                <h6>Latitude</h6>
+                                <p>{lat}</p>
+                            </li>
+                                <li className='content-para'>
+                                    <h6>Longitude</h6>
+                                    <p>{lng}</p>
+                                </li></ul>
+
+                        </div>
+                        <hr />
+                        <div className='flex' >
+                            <LiaIndustrySolid />
+                            <h4 className='flex-head' >Company</h4>
+                        </div>
+                        <div className='content'>
+                            <h3 className='content-heading'>Company Name</h3>
+                            <p className='content-para'>{company.name}</p>
+                        </div>
+                        <hr />
+                        <div className='content'>
+                            <h3 className='content-heading'>Catch Pharse</h3>
+                            <p className='content-para'>{company.catchPhrase}</p>
+                        </div>
+                        <div className='content'>
+                            <h3 className='content-heading'>Business Focus</h3>
+                            <p className='content-para'>{company.bs}</p>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div className='container-info'>
-                    <div className='flex'>
-                        <RiContactsFill />
-                        <h4 className='flex-head' >Basic Information</h4>
-                    </div>
-                    <div className='content'>
-                        <h3 className='content-heading' >User ID</h3>
-                        <p className='content-para'>{id}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Full Name</h3>
-                        <p className='content-para'>{name}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Username</h3>
-                        <p className='content-para'>{username}</p>
-                    </div>
-                    <hr />
-                    <div className='flex' >
-                        <FaPhoneAlt />
-                        <h4 className='flex-head'>Contact Information</h4>
-                    </div>
-                    <div className='content'>
-                        <h3 className='content-heading'>Email ID</h3>
-                        <p className='content-para'>{email}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Phone</h3>
-                        <p className='content-para'>{phone}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Website</h3>
-                        <p className='content-para'>{website}</p>
-                    </div>
-                    <hr />
-                    <div className='flex' >
-                        <IoLocationOutline />
-
-                        <h4 className='flex-head' >Address</h4>
-                    </div>
-                    <div className='content'>
-                        <h3 className='content-heading'>Street</h3>
-                        <p className='content-para'>{street}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Suite</h3>
-                        <p className='content-para'>{suite}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>City</h3>
-                        <p className='content-para'>{city}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Zipcode</h3>
-                        <p className='content-para'>{zipcode}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Coordinates</h3>
-                        <ul className='content-heading'><li className='content-para'>
-                            <h6>Latitude</h6>
-                            <p>{lat}</p>
-                        </li>
-                        <li className='content-para'>
-                            <h6>Longitude</h6>
-                            <p>{lng}</p>
-                        </li></ul>
-                        
-                    </div>
-                    <hr />
-                    <div className='flex' >
-                        <LiaIndustrySolid />
-                        <h4 className='flex-head' >Company</h4>
-                    </div>
-                     <div className='content'>
-                        <h3 className='content-heading'>Company Name</h3>
-                        <p className='content-para'>{company.name}</p>
-                    </div>
-                    <hr />
-                    <div className='content'>
-                        <h3 className='content-heading'>Catch Pharse</h3>
-                        <p className='content-para'>{company.catchPhrase}</p>
-                    </div>
-                     <div className='content'>
-                        <h3 className='content-heading'>Business Focus</h3>
-                        <p className='content-para'>{company.bs}</p>
-                    </div>     
+                <div>
+                    <h2>Posted by {name}</h2>
+                    <ul className='ul-items-post'>
+                        {posts.map(eachPost => (
+                            <PostItem postDetails={eachPost} key={eachPost.id} />
+                        ))}
+                    </ul>
                 </div>
-
-
             </div>
-            <div>
-                <h2>Posted by {name}</h2>
-                <ul className='ul-items-post'>
-                {posts.map(eachPost => (
+            <h1>Similar Post</h1>
+            <ul className='ul-items-similar'>
+                {similarPosts.map(eachPost => (
                     <PostItem postDetails={eachPost} key={eachPost.id} />
                 ))}
             </ul>
-            </div>
         </div>
     )
 }
--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import PostItem from '../postItem';
 import "./index.css"
 
